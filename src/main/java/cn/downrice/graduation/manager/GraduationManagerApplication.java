14693b661dd5ee5756cf55fa3928d08e626a1fd7
896882378@qq.com <qq896882378>
2017-11-27 23:33:37 +0800

    init

---
+++ src/main/java/cn/downrice/graduation/manager/GraduationManagerApplication.java
@@ -0,0 +1,12 @@
+package cn.downrice.graduation.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/downrice/graduation/manager/GraduationManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.downrice.graduation.manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraduationManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
